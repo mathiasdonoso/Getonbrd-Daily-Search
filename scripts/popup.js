187a90830a9481cfebbe4ba4bb8a0ff7ce81a5e9
mathias <mathiasd88@gmail.com>
2015-08-23 18:19:00 -0300

    modularización de helper de fechas en js

--- scripts/popup.js
+++ scripts/popup.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         $('#loader').hide();
 
-        $(data).find('ul.job-list li a').each(function(index, value) {
+        $(data).find('ul.job-list a').each(function(index, value) {
 
             var date = $(data).find('.cell.date')[index].textContent;
             var dateFormatted = date.substring(1, date.length-1)
@@ -33,31 +33,3 @@ document.addEventListener('DOMContentLoaded', function() {
         };
     });
 });
-
-function getDate() {
-    var date = new Date();
-    var months = new Array();
-    months[0] = "ene";
-    months[1] = "feb";
-    months[2] = "mar";
-    months[3] = "abr";
-    months[4] = "may";
-    months[5] = "jun";
-    months[6] = "jul";
-    months[7] = "ago";
-    months[8] = "sep";
-    months[9] = "oct";
-    months[10] = "nov";
-    months[11] = "dic";
-
-    var month = months[date.getMonth()];
-    var day = date.getDate();
-
-    if (day < 10) {
-        day = "0" + day
-    }
-
-    var finalDate = month + ' ' + day;
-
-    return finalDate;
-}
